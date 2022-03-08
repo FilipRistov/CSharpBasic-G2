f396ac2d0c9167797d78f4fae2214b3eeed2a947
FilipRistov <ristovfilip@gmail.com>
2022-03-08 20:32:08 +0100

    Add project files.

---
+++ CsharpEden/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CsharpEden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  *\n  *\n ***\n* * *\n  *\n  *\n * *\n*   *");
+
+            Console.WriteLine("Please write some text");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
+
